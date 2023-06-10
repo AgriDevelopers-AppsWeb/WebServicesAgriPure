1c4386e4bdbf770a0afaa6e7eae1a81b0ecf0c37
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-09 23:12:17 -0500

    feat/plants-api

---
+++ WebServicesAgriPure/AgriPure/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebServicesAgriPure.AgriPure.Domain.Models;
+using WebServicesAgriPure.AgriPure.Resources;
+
+namespace WebServicesAgriPure.AgriPure.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Plant, PlantResource>();
+        }
+    }
+}
---
+++ WebServicesAgriPure/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebServicesAgriPure.AgriPure.Domain.Models;
+
+namespace WebServicesAgriPure.Shared.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Plant>().ToTable("Plants");
+            builder.Entity<Plant>().HasKey(p => p.Id);
+            builder.Entity<Plant>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Plant>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+            builder.Entity<Plant>().Property(p => p.Image).IsRequired().HasMaxLength(500);
+            builder.Entity<Plant>().Property(p => p.Scientifistname).IsRequired().HasMaxLength(100);
+            builder.Entity<Plant>().Property(p => p.Variety).IsRequired().HasMaxLength(30);
+            builder.Entity<Plant>().Property(p => p.InfolandType).IsRequired().HasMaxLength(500);
+            builder.Entity<Plant>().Property(p => p.Ph).IsRequired();
+            builder.Entity<Plant>().Property(p => p.InfoDistanceBetween).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.DistancePlants).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.InfoIdealDepth).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.Depth).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.InfoWeatherConditions).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.Weather).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.InfoFertFumig).IsRequired().HasMaxLength(550);
+            builder.Entity<Plant>().Property(p => p.IntervaleFert).IsRequired();
+            builder.Entity<Plant>().Property(p => p.IntervaleFumig).IsRequired();
+            builder.Entity<Plant>().Property(p => p.SavePlant);
+        }
+    }
+}
