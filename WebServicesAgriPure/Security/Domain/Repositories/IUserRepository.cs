129d8a1427cde920388a083441261057c605834f
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2023-06-27 09:40:57 -0500

    Users

---
+++ WebServicesAgriPure/Security/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebServicesAgriPure.Security.Domain.Models;
+
+namespace WebServicesAgriPure.Security.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task AddAsync(User user);
+    Task<User> FindByIdAsync(int id);
+    Task<User> FindByUsernameAsync(string username);
+    public bool ExistsByUsername(string username);
+    User FindById(int id);
+    void Update(User user);
+    void Remove(User user);
+
+}
